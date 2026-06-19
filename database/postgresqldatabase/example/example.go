655f6e3ec0f2f1d83df873535ee67e97c1c89c1c
kesananuthana <kesananutana@gmail.com>
2026-06-19 18:47:37 +0530

    code changes

--- database/postgresqldatabase/example/example.go
+++ database/postgresqldatabase/example/example.go
@@ -202,11 +202,7 @@ func main() {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			//w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-			w.Header().Set(
-				"Access-Control-Allow-Headers",
-				"Content-Type, Authorization, X-Requested-With",
-			)
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
